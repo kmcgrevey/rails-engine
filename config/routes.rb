557e21ffdb3d7bcb4226cd7fd35e7a0fa93e90cd
Kevin McGrevey <niv3kmcg@gmail.com>
2021-06-14 10:29:46 -0600

    Returns a merchants items tested in Postman

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   
   namespace :api do
     namespace :v1 do
+      namespace :merchants do
+        get '/:id/items', to: 'items#index'
+      end
+      
       resources :merchants, only: [:show, :index]
     end
   end
