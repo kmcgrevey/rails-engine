02705e67aafa86704570ed02d821f45ea9641889
Kevin McGrevey <niv3kmcg@gmail.com>
2021-06-16 09:41:35 -0600

    Creates PageCleaner module to DRY pagination

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -9,16 +9,4 @@ class Api::V1::ItemsController < ApplicationController
     ItemSerializer
   end
 
-  def what_page
-    begin 
-      WillPaginate::PageNumber(params[:page])
-    rescue WillPaginate::InvalidPage
-      1  
-    end 
-  end
-
-  def per_page_count
-    !params[:per_page] ? 20 : params[:per_page]
-  end
-
 end
--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -14,16 +14,4 @@ class Api::V1::MerchantsController < ApplicationController
     MerchantSerializer
   end
 
-  def what_page
-    begin 
-      WillPaginate::PageNumber(params[:page])
-    rescue WillPaginate::InvalidPage
-      1  
-    end 
-  end
-
-  def per_page_count
-    !params[:per_page] ? 20 : params[:per_page]
-  end
-
 end
