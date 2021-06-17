87df9383ce0175ce9fd336af4977d931b928a2fa
Kevin McGrevey <niv3kmcg@gmail.com>
2021-06-16 20:41:19 -0600

    Add endpoint to return single Item

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -4,6 +4,11 @@ class Api::V1::ItemsController < ApplicationController
     items = Item.paginate(page: what_page, per_page: per_page_count)
     render json: serializer.new(items)
   end
+  
+  def show
+    item = Item.find(params[:id])
+    render json: serializer.new(item)
+  end
 
   def serializer
     ItemSerializer
--- spec/requests/api/v1/items/items_spec.rb
+++ spec/requests/api/v1/items/items_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Items API Endpoints', type: :request do
-  # let(:merch1) { create(:merchant) }
-  # let(:merch2) { create(:merchant) }
+
   
   describe 'returns a list of Items' do
     let!(:items) { create_list(:item, 5)}
@@ -38,6 +37,29 @@ RSpec.describe 'Items API Endpoints', type: :request do
       expect(json_data.length).to eq(5)
       expect(json_data.first[:id]).to eq(items2.first.id.to_s)
       expect(json_data.last[:id]).to eq(items2.last.id.to_s)
+    end
+  end
+
+  describe 'returns a single Item' do
+    let!(:item1) { create(:item) }
+    let!(:item2) { create(:item) }
+    
+    subject { get "/api/v1/items/#{item2.id}" }
+
+    before { subject }
+
+    it 'with a successful response' do
+      expect(response).to be_successful
+    end
+
+    it 'with a proper JSON response' do
+      expect(json_data.class).to eq(Hash)
+      expect(json_data.length).to eq(3)
+      expect(json_data[:id]).to eq(item2.id.to_s)
+      expect(json_data[:type]).to eq("item")
+      expect(json_data[:attributes][:name]).to eq(item2.name)
+      expect(json_data[:attributes][:description]).to eq(item2.description)
+      expect(json_data[:attributes][:unit_price]).to eq(item2.unit_price)
     end
   end
 
