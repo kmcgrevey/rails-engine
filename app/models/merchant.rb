50ab6ffd137abf8155d7442535b49070768f3ef3
Kevin McGrevey <niv3kmcg@gmail.com>
2021-06-28 20:05:45 -0600

    Calculates list of Merchants by most_revenue

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,10 +2,20 @@ class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
   has_many :invoice_items, through: :invoices
+  has_many :transactions, through: :invoices
 
   def self.name_finder(name)
     where('lower(name) LIKE ?', "%#{name.downcase}%")
     .order(:name)
+  end
+
+  def self.most_revenue(quantity)
+    select('merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS most_rev')
+    .joins(:invoice_items, :transactions)
+    .merge(Transaction.successful)
+    .group(:id)
+    .order('most_rev DESC')
+    .limit(quantity)
   end
 
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -7,4 +7,23 @@ RSpec.describe Merchant, type: :model do
     it { should have_many(:invoice_items).through(:invoices) }
   end
 
+  describe 'class methods' do
+    before (:each) do
+      inv_items = create_list(:invoice_item, 5)
+      inv_items.each { |item| item.invoice.transactions.create(result: 'success') }
+    end
+
+    it '.most_revenue' do
+      top_one = Merchant.most_revenue(1)
+      top_two = Merchant.most_revenue(2)
+      top_four = Merchant.most_revenue(4)
+
+      expect(top_one[1]).to eq(nil)
+      expect(top_two[0]).to eq(top_one[0])
+      expect(top_four[0]).to eq(top_one[0])
+      expect(top_four[1]).to eq(top_two[1])
+      expect(top_four[4]).to eq(nil)
+    end
+  end
+
 end
