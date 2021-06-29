6dff00c152ce386d3202764d44fd3aee7a9c7896
Kevin McGrevey <niv3kmcg@gmail.com>
2021-06-29 17:22:14 -0600

    Refactors most_revenue to include invoice status

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,7 +2,6 @@ class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
   has_many :invoice_items, through: :invoices
-  has_many :transactions, through: :invoices
 
   def self.name_finder(name)
     where('lower(name) LIKE ?', "%#{name.downcase}%")
@@ -10,9 +9,10 @@ class Merchant < ApplicationRecord
   end
 
   def self.most_revenue(quantity)
-    select('merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS rev_total')
-    .joins(:invoice_items, :transactions)
-    .merge(Transaction.successful)
+    joins(invoices: [:invoice_items, :transactions])
+    .where(transactions: { result: 'success' })
+    .where(invoices: { status: 'shipped' })
+    .select('merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS rev_total')
     .group(:id)
     .order('rev_total DESC')
     .limit(quantity)
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,4 @@
 class Transaction < ApplicationRecord
   belongs_to :invoice
 
-  scope :successful, -> { where(result: 'success')}
 end
