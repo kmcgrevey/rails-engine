4e82ee22965569a20114ddc3babe6105de1da483
Kevin McGrevey <niv3kmcg@gmail.com>
2021-07-05 16:11:09 -0600

    Adds method to calculate Potential Revenue

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,4 +3,11 @@ class Invoice < ApplicationRecord
   belongs_to :customer
   has_many :invoice_items
   has_many :transactions
+
+  def self.potential_rev
+    joins(:invoice_items)
+    .where(invoices: { status: 'packaged' })
+    .select('SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue')
+    .group(:id)
+  end
 end
